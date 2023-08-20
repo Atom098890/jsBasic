6b861e6552a57b34ed09e467e32da2cd78e62eef
Andrei <andrei@doroshenko.local>
2023-08-20 11:05:51 +0800

    add homework 5-languages switch/case

---
+++ 5-languages/homework.js
@@ -0,0 +1,16 @@
+let languages = 'es';
+
+switch (languages) {
+    case 'ru':
+        console.log('Привет!');
+        break;
+    case 'de':
+        console.log('Gutten tag!');
+        break;
+    case 'es':
+        console.log('Hola!');
+        break;
+    default:
+        console.log("I don't know this languages!");
+}
+
