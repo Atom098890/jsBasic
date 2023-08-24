58d0794460444442145c5600d89bc53f6377b16e
Andrei <andrei@doroshenko.local>
2023-08-24 10:31:29 +0800

    Правка с let на const в файле 4-navigator Переписал switch в файле 5-languages Правка условия в файле 6-driver, добавил больше либо равно 18

--- 6-driver/homework.js
+++ 6-driver/homework.js
@@ -1,6 +1,6 @@
 const hasLicence = true;
-const age = 19;
+const age = 18;
 const isDrink = false;
 
-const isDriver = age > 18 && hasLicence && !isDrink;
+const isDriver = age >= 18 && hasLicence && !isDrink;
 isDriver ? console.log('Может') : console.log('Не может');
