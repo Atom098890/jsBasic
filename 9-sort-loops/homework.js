9adee1a3d31ed1ef3a85e77224b576cb0ea0408c
Andrei <andrei@doroshenko.local>
2023-08-25 14:24:59 +0800

    Убрал лишнии переменные в файле 9-sort-loops

--- 9-sort-loops/homework.js
+++ 9-sort-loops/homework.js
@@ -6,9 +6,7 @@ function sortArray(arr) {
     for (let i = 0; i < result.length; i++) {
         for (let j = i + 1; j < result.length; j++) {
             if (result[i] > result[j]) {
-                let numderI = result[i];
-                let numberJ = result[j];
-                [result[i], result[j]] = [numberJ, numderI];
+                [result[i], result[j]] = [result[j], result[i]];
             }
         }
     }
