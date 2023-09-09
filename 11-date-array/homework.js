45542cf139ad1ed8ba95df8acb5a76672e2db792
Andrei <andrei@doroshenko.local>
2023-09-09 13:51:42 +0800

    разделил код на несколько функций и переписал логику

--- 11-date-array/homework.js
+++ 11-date-array/homework.js
@@ -1,20 +1,49 @@
-const array = ['30-06-2022', 'test', '11/31/2023', '01/00/2022', '32/12/2023'];
+const array = ['DD-MM-YYYY','MM/DD/YYYY','31-07-2022','31/31/1990','10-02-2022', '29-02-2022', '30-02-2022', '31-02-2022', 'test', 5, '11/12/2023', '00/13/2022', '41/12/2023', '41/-12/2023', 'aaaa/bbbb/vvvv', '//', [], ''];
 
-function isgValidDates(arr) {
-    const result = arr
-        .filter((item) => item.slice(0, 2) < 31 && (item.slice(3, 5) != 0 && item.slice(0, 2) != 0))
-        .map((date) => {
-            if(date.includes('/')) {
-                let num = date.split('/');
-                [num[0], num[1]] = [num[1], num[0]];
-                return num.join('-')
-            } else {
-                return date;
-            }
-        })
-        .filter((date) => !((date.slice(3, 5) == '04' || '06' || '09' || '11') && date.slice(0, 2) == 31));
+function transformDateToArray(date) {
+    let [day, month, year] = date.split('-');
 
-    return result;
+    if (date.includes('/')) {
+        [month, day, year] = date.split('/');
+    }
+
+    if (!day || isNaN(day) || isNaN(month) || isNaN(year)) {
+        return false;
+    }
+
+    return [day, month, year];
 }
 
-console.log(isgValidDates(array));
+function checkCorrectDate (str) {
+    const SHORT_MONTHS = ['04', '06', '09', '11'];
+
+    if (typeof str !== 'string') {
+        return false;
+    }   
+
+    const dateNumbersArray = transformDateToArray(str);
+
+    if (!dateNumbersArray) {
+        return false;
+    }
+
+    const [day, month] = dateNumbersArray;
+    
+    if ((Number(day) > 31 || Number(month) > 12) || (Number(day) === 0 || Number(month) === 0)) {
+        return false;
+    }
+
+    if (SHORT_MONTHS.includes(month) && day >= 31) {
+        return false;
+    }
+
+    return true;
+
+}
+
+function isValidDates(arr) {
+    const result = [...arr];
+        
+    return result.filter(checkCorrectDate).map((date) => transformDateToArray(date).join('-'));
+}
+console.log(isValidDates(array));
