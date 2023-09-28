d44b025822de10e59339efea13b6aa42ab77e73b
Andrei <andrei@doroshenko.local>
2023-09-29 07:36:32 +0800

    Сделал с addEventListener

--- 17-calc/homework.js
+++ 17-calc/homework.js
@@ -1,36 +1,44 @@
 'use strict';
 
-function calculate(action) {
-    const inputOne = document.querySelector('.inputOne').value;
-    const inputTwo = document.querySelector('.inputTwo').value;
-    if (!inputOne && !inputTwo) {
-        return;
-    }
+function calculate() {
+    document.querySelector('.action').addEventListener('click', (event) => {
+        const action = event.target.attributes[1].value
+        const inputOne = document.querySelector('.inputOne').value;
+        const inputTwo = document.querySelector('.inputTwo').value;
 
-    if (isNaN(inputOne) || isNaN(inputTwo)) {
-        alert('Введите числа в поля ввода!');
+        if (!inputOne && !inputTwo) {
+            return;
+        }
+
+        if (isNaN(inputOne) || isNaN(inputTwo)) {
+            alert('Введите числа в поля ввода!');
+            document.querySelector('.inputOne').value = '';
+            document.querySelector('.inputTwo').value = '';
+            return;
+        }
+
         document.querySelector('.inputOne').value = '';
         document.querySelector('.inputTwo').value = '';
-        return;
-    }
 
-    document.querySelector('.inputOne').value = '';
-    document.querySelector('.inputTwo').value = '';
-
-    switch(action) {
-        case '+':
-            document.querySelector('.result').innerText = `Result: ${Number(inputOne) + Number(inputTwo)}`;
-            break;
-        case '-':
-            document.querySelector('.result').innerText = `Result: ${Number(inputOne) - Number(inputTwo)}`;
-            break;
-        case '*':
-            document.querySelector('.result').innerText = `Result: ${Number(inputOne) * Number(inputTwo)}`;
-            break;
-        case '/':
-            document.querySelector('.result').innerText = `Result: ${Number(inputOne) / Number(inputTwo)}`;
-            break;
-        default:
-            console.log('Нет совпадения!');
-    }
+        switch(action) {
+            case 'addition':
+                document.querySelector('.result').innerText = `Result: ${Number(inputOne) + Number(inputTwo)}`;
+                break;
+            case 'subtraction':
+                document.querySelector('.result').innerText = `Result: ${Number(inputOne) - Number(inputTwo)}`;
+                break;
+            case 'multiplication':
+                document.querySelector('.result').innerText = `Result: ${Number(inputOne) * Number(inputTwo)}`;
+                break;
+            case 'division':
+                document.querySelector('.result').innerText = `Result: ${Number(inputOne) / Number(inputTwo)}`;
+                break;
+            default:
+                console.log('Нет совпадения!');
+        }
+    });
+    
 }
+
+calculate();
+
