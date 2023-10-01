3d8ebb2bc1a609f927fd48118b11de42f9ccf92c
Andrei <andrei@doroshenko.local>
2023-10-01 13:48:56 +0800

    Удалил функцию и заиспользовал data атрибут

--- 17-calc/homework.js
+++ 17-calc/homework.js
@@ -1,44 +1,39 @@
 'use strict';
 
-function calculate() {
-    document.querySelector('.action').addEventListener('click', (event) => {
-        const action = event.target.attributes[1].value
-        const inputOne = document.querySelector('.inputOne').value;
-        const inputTwo = document.querySelector('.inputTwo').value;
+const actionButton = document.querySelector('.action');
+actionButton.addEventListener('click', (event) => {
+    const action = event.target.dataset.action;
+    const inputOne = document.querySelector('.inputOne').value;
+    const inputTwo = document.querySelector('.inputTwo').value;
 
-        if (!inputOne && !inputTwo) {
-            return;
-        }
-
-        if (isNaN(inputOne) || isNaN(inputTwo)) {
-            alert('Введите числа в поля ввода!');
-            document.querySelector('.inputOne').value = '';
-            document.querySelector('.inputTwo').value = '';
-            return;
-        }
-
-        document.querySelector('.inputOne').value = '';
-        document.querySelector('.inputTwo').value = '';
-
-        switch(action) {
-            case 'addition':
-                document.querySelector('.result').innerText = `Result: ${Number(inputOne) + Number(inputTwo)}`;
-                break;
-            case 'subtraction':
-                document.querySelector('.result').innerText = `Result: ${Number(inputOne) - Number(inputTwo)}`;
-                break;
-            case 'multiplication':
-                document.querySelector('.result').innerText = `Result: ${Number(inputOne) * Number(inputTwo)}`;
-                break;
-            case 'division':
-                document.querySelector('.result').innerText = `Result: ${Number(inputOne) / Number(inputTwo)}`;
-                break;
-            default:
-                console.log('Нет совпадения!');
-        }
-    });
-    
+if (!inputOne && !inputTwo) {
+    return;
 }
 
-calculate();
+if (isNaN(inputOne) || isNaN(inputTwo)) {
+    alert('Введите числа в поля ввода!');
+    document.querySelector('.inputOne').value = '';
+    document.querySelector('.inputTwo').value = '';
+    return;
+}
 
+document.querySelector('.inputOne').value = '';
+document.querySelector('.inputTwo').value = '';
+
+switch(action) {
+    case 'addition':
+        document.querySelector('.result').innerText = `Result: ${Number(inputOne) + Number(inputTwo)}`;
+        break;
+    case 'subtraction':
+        document.querySelector('.result').innerText = `Result: ${Number(inputOne) - Number(inputTwo)}`;
+        break;
+    case 'multiplication':
+        document.querySelector('.result').innerText = `Result: ${Number(inputOne) * Number(inputTwo)}`;
+        break;
+    case 'division':
+        document.querySelector('.result').innerText = `Result: ${Number(inputOne) / Number(inputTwo)}`;
+        break;
+    default:
+        console.log('Нет совпадения!');
+}
+});
